5803a758948e0a08f6a238d2d1ca87f0097cee74
Tom Sain <tom.sain@Nology.local>
2019-05-09 17:06:52 +0100

    pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+// add an event listener to the text box you type the text you wish to TRANSLATE in
+document.getElementById("translateFrom").addEventListener("change", translate);
+
+// define the function
+const translate = () => {};
+
+// output event.target.value to the console to see if event listener works
+console.log();
+
+// print the event.target.value to the screen using getelementbyid
+
+//
